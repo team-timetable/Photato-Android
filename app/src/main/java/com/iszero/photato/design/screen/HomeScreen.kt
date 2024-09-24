3ce5ea0c92773455fd13c367eb6498bac9440401
iszero12 <binj1246@gmail.com>
2024-09-24 23:51:14 +0900

    feat:: 홈화면, 마이화면 파일생성

--- app/src/main/java/com/iszero/photato/design/screen/HomeScreen.kt
+++
@@ -1,9 +0,0 @@
-package com.iszero.photato.design.screen
-
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-
-@Composable
-fun HomeScreen(){
-    Text(text = "홈화면임")
-}
--- app/src/main/java/com/iszero/photato/design/screen/MyScreen.kt
+++
@@ -1,9 +0,0 @@
-package com.iszero.photato.design.screen
-
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-
-@Composable
-fun MyScreen(){
-    Text(text = "마이스크린")
-}
